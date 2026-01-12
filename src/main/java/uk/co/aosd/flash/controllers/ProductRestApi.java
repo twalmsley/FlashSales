9d51f7e61b231586dc54dd03e78db9c2c23beed6
Tony <tony@aosd.co.uk>
2026-01-12 14:32:24 +0000

    Enabled liveness/readiness probes. Added another profile.

--- src/main/java/uk/co/aosd/flash/controllers/ProductRestApi.java
+++ src/main/java/uk/co/aosd/flash/controllers/ProductRestApi.java
@@ -2,6 +2,7 @@ package uk.co.aosd.flash.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
+@Profile("admin-service")
 @RequestMapping("/api/v1/products")
 public class ProductRestApi {
 
